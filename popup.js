5718b9a18a2ffe1a975a239aa9c59475b8897a5f
Allen Chang <allensylverostchang@gmail.com>
2019-01-12 12:54:12 -0800

    Fix e

--- popup.js
+++ popup.js
@@ -1,5 +1,7 @@
 
-document.getElementById("loginGrid").onsubmit = function(){
+document.getElementById("loginGrid").onsubmit = function(e){
+    e.preventDefault();
+
     document.getElementById("loginGrid").style.display = "none";
     document.getElementById("main").style.display = "grid";
 }
