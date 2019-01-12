caa6c19fb67bbac116cd06ab502a742273cbc29d
Allen Chang <allensylverostchang@gmail.com>
2019-01-12 12:50:09 -0800

    Login Grid

---
+++ popup.js
@@ -0,0 +1,5 @@
+
+document.getElementById("loginGrid").onsubmit = function(){
+    document.getElementById("loginGrid").style.display = "none";
+    document.getElementById("main").style.display = "grid";
+}
