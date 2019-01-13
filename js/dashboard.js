4f6c8a8777854799162ca0c344f908598b4e5c2f
Allen Chang <allensylverostchang@gmail.com>
2019-01-13 00:44:03 -0800

    Stylize Dashboard Search

--- js/dashboard.js
+++ js/dashboard.js
@@ -9,48 +9,49 @@ var config = {
 firebase.initializeApp(config);
 var url = "https://pokeapi.co/api/v2/pokemon/";
 db = firebase.firestore();
-var username = localStorage.getItem("username");
-var password = localStorage.getItem("password");
+
 $(document).ready(function() {
-    getData(renderData);
+    getData(localStorage.getItem("username"), document.getElementById("userGrid"));
 })
-function getData(callback) {
+function getData(username, container) {
+    while(container.firstChild) 
+        container.removeChild(container.firstChild);
+
     var userDoc = db.collection("users").doc(username);
     userDoc.get().then(function(doc) {
         money = doc.data().pokecoins;
         party = doc.data().party;
-        callback(party);
+        renderData(party, container);
     })
 }
 function updateData(money, party) {
-    var userDoc = db.collection("users").doc(username);
+    var userDoc = db.collection("users").doc(localStorage.getItem("username"));
     userDoc.update({
         money: money,
         party: party
     })
 }
-function renderData(party) {
-    var partyGrid = document.getElementById("partyGrid");
+function renderData(party, container) {
     for(var i = 0; i < party.length; i++) {
         var containsAlready = false;
         var j = 0;
-        for (j = 0; !containsAlready && j < partyGrid.children.length; j++) {
-            if (partyGrid.children.item(j).innerHTML.toLowerCase() === party[i].name)
+        for (j = 0; !containsAlready && j < container.children.length; j++) {
+            if (container.children.item(j).innerHTML.toLowerCase() === party[i].name)
                 containsAlready = true;
         }
         j--;
 
         if (containsAlready){
-            var countstr = partyGrid.children.item(j).getAttribute("count");
+            var countstr = container.children.item(j).getAttribute("count");
             var count = parseInt(countstr.substr(1, countstr.length - 2));
-            partyGrid.children.item(j).setAttribute("count", "(" + (count + 1).toString() + ")");
+            container.children.item(j).setAttribute("count", "(" + (count + 1).toString() + ")");
         } else {
             var name = document.createElement('div');
             name.setAttribute('class','left');
             name.setAttribute('id', 'pokename');
             name.setAttribute('count', "(1)");
             name.innerHTML = party[i].name.charAt(0).toUpperCase() + party[i].name.substr(1, party[i].name.length - 1);
-            partyGrid.appendChild(name);
+            container.appendChild(name);
 
             var icon = document.createElement('div');
             icon.setAttribute('class','right');
@@ -59,7 +60,12 @@ function renderData(party) {
             img.style.width = "40px";
             img.style.height = "40px";
             icon.appendChild(img);
-            partyGrid.appendChild(icon);
+            container.appendChild(icon);
         }
     }
+}
+
+document.getElementById("searchSubmit").onclick = function(e){
+    e.preventDefault();
+    getData(document.getElementById("search-username").value, document.getElementById("searchGrid"));
 }
