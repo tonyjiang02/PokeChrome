52bb6a11770a78e1f046ff9ca6d4a49880f8a4f7
bruisedllama <tonyjiang02@gmail.com>
2019-01-13 00:48:43 -0800

    marketplace

--- js/dashboard.js
+++ js/dashboard.js
@@ -13,12 +13,13 @@ var party;
 var money;
 var username = localStorage.getItem("username");
 var password = localStorage.getItem("password");
-$(document).ready(function() {
+$(document).ready(function () {
     getData(renderData);
+    renderMarketplace();
 })
 function getData(callback) {
     var userDoc = db.collection("users").doc(username);
-    userDoc.get().then(function(doc) {
+    userDoc.get().then(function (doc) {
         money = doc.data().pokecoins;
         party = doc.data().party;
         console.log(party);
@@ -35,21 +36,75 @@ function updateData() {
 }
 function renderData() {
     var container = document.getElementById("container");
-    for(var i =0; i<party.length; i++) {
+    for (var i = 0; i < party.length; i++) {
         var row = document.createElement('div');
-        row.setAttribute('class','row');
+        row.setAttribute('class', 'row');
         var col1 = document.createElement('div');
-        col1.setAttribute('class','col-md-2');
+        col1.setAttribute('class', 'col-md-2');
         var col2 = document.createElement('div');
-        col2.setAttribute('class','col-md-4');
-        col1.innerHTML =party[i].name;
+        col2.setAttribute('class', 'col-md-4');
+        col1.innerHTML = party[i].name;
         var img = document.createElement('img');
         img.setAttribute('src', party[i].sprite);
         img.style.width = "40px";
         img.style.height = "40px";
         col2.appendChild(img);
         row.appendChild(col1);
-        row.appendChild(col2); 
+        row.appendChild(col2);
         container.appendChild(row);
+    }
+}
+function addSale(seller, pokemon, price) {
+    var forSale = db.collection("marketplace").doc("forSale");
+    var uuid = guid();
+    var obj = {
+        seller:seller,
+        pokemon:pokemon,
+        price:price,
+        reference:uuid
+    }
+    forSale.update({
+        uuid:obj
+    })
+}
+function renderMarketplace() {
+    var forSale = db.collection("marketplace").doc("forSale");
+    forSale.onSnapshot({
+        includeMetadataChanges: true
+    }, function (doc) {
+        clearMarket();
+        var market = doc.data();
+        for (var key in market) {
+            var sell = market[key];
+            console.log(sell);
+            var div = document.createElement('div');
+            var seller = document.createElement('p');
+            var pokemon = document.createElement('p');
+            var price = document.createElement('p');
+            var button = document.createElement('button')
+            seller.innerHTML = sell.seller;
+            pokemon.innerHTML = sell.pokemon;
+            price.innerHTML = sell.price;
+            div.setAttribute('reference', sell.reference);
+            button.innerHTML = "Buy Pokemon";
+            div.appendChild(seller);
+            div.appendChild(pokemon);
+            div.appendChild(price);
+            div.appendChild(button);
+        }
+    });
+}
+function guid() {
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1);
+    }
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+}
+function clearMarket() {
+    var market = document.getElementById("marketplace");
+    while (market.firstChild) {
+        market.removeChild(market.firstChild);
     }
 }
