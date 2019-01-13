07e5e20ee2abd3a1fa4387aa087943d256759fa5
Allen Chang <allensylverostchang@gmail.com>
2019-01-12 23:37:06 -0800

    Stylize Dashboard

--- js/dashboard.js
+++ js/dashboard.js
@@ -9,8 +9,6 @@ var config = {
 firebase.initializeApp(config);
 var url = "https://pokeapi.co/api/v2/pokemon/";
 db = firebase.firestore();
-var party;
-var money;
 var username = localStorage.getItem("username");
 var password = localStorage.getItem("password");
 $(document).ready(function() {
@@ -21,35 +19,47 @@ function getData(callback) {
     userDoc.get().then(function(doc) {
         money = doc.data().pokecoins;
         party = doc.data().party;
-        console.log(party);
-        console.log(money);
-        callback();
+        callback(party);
     })
 }
-function updateData() {
+function updateData(money, party) {
     var userDoc = db.collection("users").doc(username);
     userDoc.update({
         money: money,
         party: party
     })
 }
-function renderData() {
-    var container = document.getElementById("container");
-    for(var i =0; i<party.length; i++) {
-        var row = document.createElement('div');
-        row.setAttribute('class','row');
-        var col1 = document.createElement('div');
-        col1.setAttribute('class','col-md-2');
-        var col2 = document.createElement('div');
-        col2.setAttribute('class','col-md-4');
-        col1.innerHTML =party[i].name;
-        var img = document.createElement('img');
-        img.setAttribute('src', party[i].sprite);
-        img.style.width = "40px";
-        img.style.height = "40px";
-        col2.appendChild(img);
-        row.appendChild(col1);
-        row.appendChild(col2); 
-        container.appendChild(row);
+function renderData(party) {
+    var partyGrid = document.getElementById("partyGrid");
+    for(var i = 0; i < party.length; i++) {
+        var containsAlready = false;
+        var j = 0;
+        for (j = 0; !containsAlready && j < partyGrid.children.length; j++) {
+            if (partyGrid.children.item(j).innerHTML.toLowerCase() === party[i].name)
+                containsAlready = true;
+        }
+        j--;
+
+        if (containsAlready){
+            var countstr = partyGrid.children.item(j).getAttribute("count");
+            var count = parseInt(countstr.substr(1, countstr.length - 2));
+            partyGrid.children.item(j).setAttribute("count", "(" + (count + 1).toString() + ")");
+        } else {
+            var name = document.createElement('div');
+            name.setAttribute('class','left');
+            name.setAttribute('id', 'pokename');
+            name.setAttribute('count', "(1)");
+            name.innerHTML = party[i].name.charAt(0).toUpperCase() + party[i].name.substr(1, party[i].name.length - 1);
+            partyGrid.appendChild(name);
+
+            var icon = document.createElement('div');
+            icon.setAttribute('class','right');
+            var img = document.createElement('img');
+            img.setAttribute('src', party[i].sprite);
+            img.style.width = "40px";
+            img.style.height = "40px";
+            icon.appendChild(img);
+            partyGrid.appendChild(icon);
+        }
     }
 }
